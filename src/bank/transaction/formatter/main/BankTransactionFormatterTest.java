f844ae481148d714c2c5cb15eb2dcf793a1920c1
Kyle Watson <kyle.scott.watson@gmail.com>
2015-10-05 14:34:58 -0400

    Created high level test file with default failing test

---
+++ src/bank/transaction/formatter/main/BankTransactionFormatterTest.java
@@ -0,0 +1,14 @@
+package bank.transaction.formatter.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankTransactionFormatterTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
