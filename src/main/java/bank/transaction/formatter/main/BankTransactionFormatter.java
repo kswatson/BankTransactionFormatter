fae6586512c9722157cc2190ae14106005f6785e
Kyle Watson <kyle.scott.watson@gmail.com>
2016-03-26 16:13:12 -0400

    Removed no longer needed PC_FINANCIAL_HEADER constant

--- src/main/java/bank/transaction/formatter/main/BankTransactionFormatter.java
+++ src/main/java/bank/transaction/formatter/main/BankTransactionFormatter.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public class BankTransactionFormatter {
 
-	public static final String PC_FINANCIAL_HEADER = "Date, Transaction Details, Funds Out, Funds In ";
-
 	public static void main(String[] args) throws IOException {
 
 		String inputFileName = args[0];
