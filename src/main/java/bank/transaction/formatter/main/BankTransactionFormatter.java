e34796ef354f4984396cfe08c6a7cd8c86587664
Kyle Watson <kyle.scott.watson@gmail.com>
2016-03-26 13:45:26 -0400

    BankTransactionFormatter now writes data to file

--- src/main/java/bank/transaction/formatter/main/BankTransactionFormatter.java
+++ src/main/java/bank/transaction/formatter/main/BankTransactionFormatter.java
@@ -2,6 +2,9 @@ package bank.transaction.formatter.main;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BankTransactionFormatter {
 
@@ -9,6 +12,9 @@ public class BankTransactionFormatter {
 		File output = new File("output.csv");
 
 		output.createNewFile();
+		List<String> lines = new ArrayList<String>();
+		lines.add("Hello,World");
+		Files.write(output.toPath(), lines);
 
 	}
 
--- src/test/java/bank/transaction/formatter/main/BankTransactionFormatterTest.java
+++ src/test/java/bank/transaction/formatter/main/BankTransactionFormatterTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,13 +22,26 @@ public class BankTransactionFormatterTest {
 	}
 
 	@Test
-	public void bankTransactionFormatterShouldReturnCSV() throws IOException {
+	public void bankTransactionFormatterShouldReturnCsv() throws IOException {
 		String[] args = new String[] { "testInput.csv" };
 		BankTransactionFormatter.main(args);
 
 		File outputFile = new File("output.csv");
 
 		assertTrue(outputFile.exists());
+	}
+
+	@Test
+	public void bankTransactionFormatterShouldReturnCsvWithData() throws IOException {
+		String[] args = new String[] { "testInput.csv" };
+		BankTransactionFormatter.main(args);
+
+		File outputFile = new File("output.csv");
+
+		assertTrue(outputFile.exists());
+		List<String> lines = Files.readAllLines(outputFile.toPath());
+
+		assertTrue(!lines.isEmpty());
 	}
 
 }
