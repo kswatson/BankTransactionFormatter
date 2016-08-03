d69e78e51cb64ed708f51fb0a9f157bb57cd6152
kyles <kyles@192.168.0.144>
2016-08-03 19:31:02 -0400

    Added tests for buildOutputLines and refactored some constants

--- src/test/java/bank/transaction/formatter/main/parser/PcfTransactionCsvParserTest.java
+++ src/test/java/bank/transaction/formatter/main/parser/PcfTransactionCsvParserTest.java
@@ -13,19 +13,52 @@ import bank.transaction.formatter.main.domain.PcfTransactionFile;
 
 public class PcfTransactionCsvParserTest {
 
+	private static final String TEST_TRANSACTION_2 = "02/02/2016, Test funds out transaction,10.50,";
+	private static final String TEST_TRANSACTION_1 = "02/01/2016, Test funds in transaction,,225.72";
+	private static final String TEST_HEADER = "Date, Transaction Details, Funds Out, Funds In";
 	private static final String TEST_PC_FINANCIAL_INPUT_FILE_NAME = "PCF.csv";
 	
 	@Test
 	public void parsePcfTransactionCsvReturnsTransactionFile() throws IOException {
-		String expectedHeader = "Date, Transaction Details, Funds Out, Funds In";
-		List<String> expectedTransactions = new ArrayList<String>();
-		expectedTransactions.add("02/01/2016, Test funds in transaction,,225.72");
-		expectedTransactions.add("02/02/2016, Test funds out transaction,10.50,");
+		String expectedHeader = TEST_HEADER;
+		List<String> expectedTransactions = createTestTransactions();
 		
 		PcfTransactionFile actual = PcfTransactionCsvParser.parsePcfTransactionCsv(TEST_PC_FINANCIAL_INPUT_FILE_NAME);
 		
 		assertNotNull(actual);
 		assertEquals(expectedHeader, actual.getHeader());
 		assertEquals(expectedTransactions, actual.getTransactions());
+	}
+
+	@Test
+	public void buildOutputLinesAddsHeader() {
+		String expectedHeader = TEST_HEADER;
+		List<String> expectedTransactions = createTestTransactions();
+		
+		PcfTransactionFile expectedFile = new PcfTransactionFile(expectedHeader, expectedTransactions);
+		
+		List<String> actual = PcfTransactionCsvParser.buildOutputLines(expectedFile);
+		
+		assertEquals(expectedHeader, actual.get(0));
+	}
+	
+	@Test
+	public void buildOutputLinesAddsTransactions() {
+		String expectedHeader = TEST_HEADER;
+		List<String> expectedTransactions = createTestTransactions();
+		
+		PcfTransactionFile expectedFile = new PcfTransactionFile(expectedHeader, expectedTransactions);
+		
+		List<String> actual = PcfTransactionCsvParser.buildOutputLines(expectedFile);
+		actual.remove(0);
+
+		assertEquals(expectedTransactions, actual);
+	}
+	
+	private List<String> createTestTransactions() {
+		List<String> expectedTransactions = new ArrayList<String>();
+		expectedTransactions.add(TEST_TRANSACTION_1);
+		expectedTransactions.add(TEST_TRANSACTION_2);
+		return expectedTransactions;
 	}
 }
